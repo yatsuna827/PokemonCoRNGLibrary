46ee0a611eb718eaf598cecc63faf15311985862
yatsuna827 <yatsuna_827@yahoo.co.jp>
2020-12-18 23:21:03 +0900

    ID関係の逆算処理を追加

--- PokemonCoRNGLibrary/CoStarter.cs
+++ PokemonCoRNGLibrary/CoStarter.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using PokemonPRNG.LCG32;
 using PokemonPRNG.LCG32.GCLCG;
 using PokemonStandardLibrary;
@@ -24,6 +24,56 @@ namespace PokemonCoRNGLibrary
             return new CoStarterResult(TID, SID, e, u);
         }
 
+        // なんでもワンライナーにすればいいってもんじゃない.
+        public IEnumerable<uint> CalcBack(uint tid, uint sid)
+            => LOWER[(sid - (0x43FDU * tid)) & 0xFFFF].Select(_ => ((tid << 16) | _).PrevSeed(1001)).Where(_ => IsAccessible(_));
+
+        public IEnumerable<uint> CalcBack(uint tid, uint pid, ShinyType shinyType)
+        {
+            // 死ね.
+            if (shinyType == ShinyType.NotShiny) yield break;
+
+            var psv = (pid >> 16) ^ (pid & 0xFFFF);
+
+            // 菱形になるseedを返す.
+            if ((shinyType & ShinyType.Square) != 0)
+                foreach (var seed in CalcBack(tid, tid ^ psv)) yield return seed;
+            
+            // 星型になるseedを返す.
+            if((shinyType & ShinyType.Star) != 0)
+                for(uint bit =1; bit < 8; bit++)
+                    foreach (var seed in CalcBack(tid, tid ^ psv ^ bit)) yield return seed;
+        }
+
+        private static readonly uint[][] LOWER;
+        static CoStarterGenerator()
+        {
+            var lower = Enumerable.Range(0, 0x10000).Select(_ => new List<uint>()).ToArray();
+            for (uint y = 0; y < 0x10000; y++) lower[(y.NextSeed() >> 16)].Add(y);
+
+            LOWER = lower.Select(_ => _.ToArray()).ToArray();
+        }
+
+        // 名前画面の不定消費を考慮して実現できるかどうか. 
+        // ID決定までに最低でも数百消費は入るのでコーナーケースは無視してよい.
+        // 再帰でBack()の呼び出し/条件判定に若干の無駄はあるものの、どうせそんなに回数呼ばれないので誤差だと思っていい.
+        // https://twitter.com/sub_827/status/1339884393494532096
+        private static bool IsAccessible(uint seed)
+        {
+            var prev1 = seed > 0x1999FFFF; // a[i-1]
+            var prev2 = seed.Back() > 0x1999FFFF; // a[i-2]
+            var prev3 = seed.Back() > 0x1999FFFF; // a[i-3]
+            var prev4 = seed.Back() > 0x1999FFFF; // a[i-4]
+
+            if (prev1 && prev2 && prev3 && prev4) return true; // 直前の4つがスキップ条件満たさないなら安全.
+
+            if (seed.Back() < 0x199A0000 && IsAccessible(seed.PrevSeed())) return true;
+            if (seed.Back() < 0x199A0000 && prev1 && IsAccessible(seed.PrevSeed())) return true;
+            if (seed.Back() < 0x199A0000 && prev1 && prev2 && IsAccessible(seed.PrevSeed())) return true;
+            if (seed.Back() < 0x199A0000 && prev1 && prev2 && prev3 && IsAccessible(seed.PrevSeed())) return true;
+
+            return false;
+        }
     }
     public class CoStarterResult
     {
