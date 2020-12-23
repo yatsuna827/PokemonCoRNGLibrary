d2003fd2e99e23903cf129b32c1f75a4a6c0f912
yatsuna827 <yatsuna_827@yahoo.co.jp>
2020-12-24 02:58:42 +0900

    ギンザルプラスルを追加

---
+++ PokemonCoRNGLibrary/CoTradePokemon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PokemonPRNG.LCG32;
+using PokemonPRNG.LCG32.GCLCG;
+using PokemonStandardLibrary;
+
+namespace PokemonCoRNGLibrary
+{
+    public class CoTradePokemon : IGeneratable<GCIndividual>
+    {
+        public readonly GCSlot slot;
+        public readonly uint TID, SID;
+
+        public static readonly CoTradePokemon Plusle = new CoTradePokemon("プラスル", 13, 37149, 00000);
+
+        public GCIndividual Generate(uint seed) => slot.Generate(seed, out _, TID ^ SID);
+        public IEnumerable<(uint seed, GCIndividual Individual)> CalcBack(uint h, uint a, uint b, uint c, uint d, uint s) => SeedFinder.FindGeneratingSeed(h, a, b, c, d, s, false).Select(_ => (_, Generate(_)));
+
+        private CoTradePokemon(string name, uint lv, uint tid, uint sid)
+        {
+            slot = new GCSlot(name, lv);
+            TID = tid;
+            SID = sid;
+        }
+    }
+}
