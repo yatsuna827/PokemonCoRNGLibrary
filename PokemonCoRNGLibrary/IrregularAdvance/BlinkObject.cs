22debdbec04a25501baa8389526ebc78f4360636
yatsuna827 <yatsuna_827@yahoo.co.jp>
2020-12-25 04:42:13 +0900

    瞬きシミュレート用のクラスを作成

---
+++ PokemonCoRNGLibrary/IrregularAdvance/BlinkObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PokemonPRNG.LCG32.GCLCG;
+
+namespace PokemonCoRNGLibrary.IrregularAdvance
+{
+    class BlinkObject
+    {
+        private readonly int coolTime;
+        private int remainCoolTime = 0;
+        private int blinkCounter;
+
+        public void Initialize()
+        {
+            blinkCounter = 0;
+            remainCoolTime = 0;
+        }
+        public bool CountUp(ref uint seed, ref uint index)
+        {
+            if (remainCoolTime-- > 0) return false; // マイナスに振り切るぶんには問題ないので.
+            if ((blinkCounter += 2) < 10) return false;
+
+            index++;
+            if (seed.GetRand() >= BlinkConst.blinkThresholds[blinkCounter - 10]) return false;
+
+            blinkCounter = 0;
+            remainCoolTime = coolTime;
+
+            return true;
+        }
+        public BlinkObject(int cool = 4) { coolTime = cool; Initialize(); }
+    }
+}
